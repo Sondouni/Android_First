3a85d0dcba5bcce97580451966710b8408c70900
Sondouni <ezicland@gmail.com>
2021-10-15 17:23:19 +0900

    2021-10-15

---
+++ app/src/main/java/com/example/sondouni/MenuActivity.java
@@ -0,0 +1,83 @@
+package com.example.sondouni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+    public void moveToActivity(View v){
+        int id = v.getId();
+        Class c = null;
+        switch (id){
+            case R.id.menuBtn1:
+                c=MainActivity.class;
+                break;
+            case R.id.menuBtn2:
+                c=LinearActivity.class;
+                break;
+            case R.id.menuBtn3:
+                c=ConstraintActivity.class;
+                break;
+        }
+
+        Intent intent = new Intent(this,c);
+        startActivity(intent);
+    }
+    public void call(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1111-2222"));
+        startActivity(intent);
+    }
+    public void moveToActivityText(View v){
+        TextView tv = (TextView)v;
+        String text = (String) tv.getText();
+        Log.i("myLog",text);
+
+        Class c = null;
+//        if("Main".equals(text))(
+//            c=MainActivity.class;
+//        )else if
+        switch (text){
+                case "Main":
+                   c=MainActivity.class;
+                    break;
+               case "Linear":
+                   c=LinearActivity.class;
+                    break;
+                case "Constraint":
+                    c=ConstraintActivity.class;
+                   break;
+            }
+
+
+
+        Intent intent = new Intent(this,c);
+        startActivity(intent);
+
+//        Intent intent = new Intent(this,c);
+//        startActivity(intent);
+    }
+//    public void moveToMain(View v){
+//        Intent intent = new Intent(this,MainActivity.class);
+//        startActivity(intent);
+//    }
+//    public void moveToLinear(View v){
+//        Intent intent = new Intent(this,LinearLayout.class);
+//        startActivity(intent);
+//    }
+//    public void moveToConstraint(View v){
+//        Intent intent = new Intent(this, ConstraintActivity.class);
+//        startActivity(intent);
+//    }
+}
